3c0cdb4a9b4fa99be8e3db9b70a01d977119ccd8
Your NameMike <waleboluwatife@gail.com>
2022-06-02 16:51:24 +0100

    First Commit

---
+++ AkandeMichael/AkandeMichael/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for AkandeMichael project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AkandeMichael.settings')
+
+application = get_asgi_application()
